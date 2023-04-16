2ac3d5e9a3330dc6221b14d4c396e2321aad6aa8
cartini-dominic <dominic@cartini.com>
2023-04-16 16:45:11 +0900

    nestjs upload

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+  .setTitle('뇌절PT API')
+  .setDescription('This is 뇌절PT API description')
+  .setVersion('0.0.0')
+  .addTag('Projdct Nonamed')
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api-docs', app, document)
+  
+  await app.listen(3000);
+}
+bootstrap();
