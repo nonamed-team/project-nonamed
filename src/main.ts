f56b7698e0fc202580f21ec5858a6ac611d25aff
cartini-dominic <dominic@cartini.com>
2023-06-06 16:41:20 +0900

    fix. cors 설정 변경

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('overdo')
